f66450700732c99c238e64d696d493ca62216d4d
saidmuhamedovsaidakbar27@gmail.com <Brat711>
2024-12-02 22:01:40 +0500

    alosad

---
+++ main.js
@@ -0,0 +1,144 @@
+function qiyirq() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta qiyqiriq narhi ${zakaz * 45000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function snec() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta snec narhi ${zakaz * 79000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function klasik() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta klasik narhi ${zakaz * 34000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function longer() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta longer narhi ${zakaz * 45000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function big() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta big narhi ${zakaz * 62000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function lester() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta lester narhi ${zakaz * 53000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function Скул() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta Скул  narhi ${zakaz * 36000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function Комбо() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta Комбо  narhi ${zakaz * 20000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function Dostlar1х() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta Do’stlar 1х  narhi ${zakaz * 43000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function Dostlar2х() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta Dostlar2х  narhi ${zakaz * 79000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function Dostlar() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta Dostlar narhi ${zakaz * 45000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
+function ОстрыйDostlar() {
+    const zakaz = +prompt("Nechta zakaz?");
+    const result = confirm(`${zakaz} ta  Острый Dostlar narhi ${zakaz * 43000} som boladi olasizmi`)
+    switch (result) {
+        case true:
+            alert("Zakaz qabul qilindi");
+            break;
+        case false:
+            alert("Zakaz qabul qilinmadi");
+            break;
+    }
+}
